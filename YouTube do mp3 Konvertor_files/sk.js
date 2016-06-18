3836e28780f5590bb28366075b0859ce46f37d82
Andy Vanellus <andrejcibik@gmail.com>
2016-06-18 09:42:36 +0200

    first

---
+++ YouTube do mp3 Konvertor_files/sk.js
@@ -0,0 +1,21 @@
+﻿var _ytmp3Lang = { 	
+'INVALID_URL' : 'Neplatná URL',
+'MAINTENANCE' : 'Práve prebieha údržba. Všetky naše servery sú v režime – “iba na čítanie”. Prosím skúste znova v priebehu ďaľšej hodiny.',
+//'MAINTENANCE' : '<img src="/smiley.gif" style="float:left; width:109px; height:75px" /><div style="float:left; margin-top:8px; margin-left:10px; font-size:13px;"><i>Google Inc.</i> doesn\'t want you to convert this video.<br>Please sign a petition to change their mind:<br><a href="http://www.change.org/petitions/google-inc-youtube-llc-allow-third-party-recording-tools-for-youtube" target="_blank">Support third party recording tools for YouTube</a></div>',
+'ERROR' : 'Stala sa chyba, zapríčinená YouTube, nemôžeme preniesť toto video! Väčšinou je táto chyba zapríčinená autorskými právami alebo dĺžkou videa. Podporujeme iba videá s dĺžkou maximálne 20 minút.',
+'LIMIT' : 'Limit dosiahnutý! Povoľujeme 15 konverzií za pol hodinu. Keď si myslíte, že tento limit je príliž nízky, prosím kontaktujte nás',
+
+'TITLE' : '<b>Titul:</b> $0',
+'LENGTH' : '<b>Dĺžka:</b> $0 minúty',
+'CONVERTING' : 'Sťahovanie ukončené: Konverzia videa',
+'PROGRESS' : '$0% stiahnuté ($1 kB/s)',
+'CONVERTED' : 'Video bolo úspešne konvertované na mp3',
+'DOWNLOAD' : 'Stiahni',
+'PROCESSING' : 'Spracovanie videa',
+
+'LINKIT' : 'Link na toto mp3',
+'SENDIT' : 'Send this mp3',
+'BBCODELINK' : 'BB Code (Forums)',
+'HTMLCODELINK' : 'HTML Code',
+'DIRECTCODELINK' : 'Direct Link'
+};
